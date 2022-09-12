c6b9598fae99de72d1464637fc2a6b624d293887
sagarciaescobar <sa.garciaescobar@gmail.com>
2022-09-12 16:45:58 -0500

    Add metadata token

--- pages/user/tokens/[id]/index.js
+++ pages/user/tokens/[id]/index.js
@@ -1,52 +1,38 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DetailPageContainer, DetailContainer } from './index.module.css';
-import DetailOptions from '../../../../components/DetailOptions'
-import DetailToken from '../../../../components/DetailToken'
+import DetailOptions from '../../../../components/DetailOptions';
+import DetailToken from '../../../../components/DetailToken';
+import { useNeoFutbol } from '../../../../hooks/useNeoFutbol';
+import { useRouter } from 'next/router';
+import LoaderSpinner from '../../../../components/LoaderSpinner';
 
 export default function Detail() {
-        const mockPlayer = {
-          name: 'ACUÑA',
-          image: 'https://playersnft-b264c.web.app/players/argentina/default/001.jpg',
-          attributes: [
-            {
-              'trail-type': 'PAC',
-              value: '76',
-            },
-            {
-              'trail-type': 'SHO',
-              value: '74',
-            },
-            {
-              'trail-type': 'PAS',
-              value: '82',
-            },
-            {
-              'trail-type': 'DRI',
-              value: '87',
-            },
-            {
-              'trail-type': 'DEF',
-              value: '79',
-            },
-            {
-              'trail-type': 'PHY',
-              value: '82',
-            },
-          ],
-        };
+  const router = useRouter();
+  const { query } = router;
+  const [token, setToken] = useState({});
+  const { contract, loading, setLoading } = useNeoFutbol();
+  useEffect(() => {
+    if (contract?.getNftById && query.id) {
+      setLoading(true);
+      contract.getNftById(query['id']).then(data => setToken(data));
+      setLoading(false);
+    }
+  }, [contract?.getNftById, query]);
 
-    return (
-        <div className={DetailPageContainer}>
-            <Head>
-            <title>NFT Neo FuTbol</title>
-            <meta name="description" content="NFT Neo FuTbol" />
-            <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={DetailContainer} style={{ height: '100vh' }}>
-            <DetailToken dataToken={mockPlayer} />
-            <DetailOptions />
-            </div>
-        </div>
-    );
+  if (loading) return <LoaderSpinner />;
+
+  return (
+    <div className={DetailPageContainer}>
+      <Head>
+        <title>NFT Neo FuTbol</title>
+        <meta name="description" content="NFT Neo FuTbol" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={DetailContainer} style={{ height: '100vh' }}>
+        <DetailToken dataToken={token} />
+        <DetailOptions />
+      </div>
+    </div>
+  );
 }
